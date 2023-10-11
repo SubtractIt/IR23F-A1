10ce8ccdde8a8c5be1eb8212e321a7e9085ea908
Adit Mohindra <aditmohindra@gmail.com>
2023-10-11 15:14:28 -0700

    ignores non english

--- PartA.py
+++ PartA.py
@@ -7,10 +7,9 @@ def tokenize(filename):
     tokens = []
     with open(filename, 'r', encoding='utf-8') as data_file:
         for line in data_file:
-            # Use regular expression to split text by non-alphanumeric characters
-            words = re.findall(r'\w+', line)
-            for word in words:
-                tokens.append(word.lower())
+            # Use regular expression to match English words
+            english_words = re.findall(r'\b[a-zA-Z]+\b', line)
+            tokens.extend(word.lower() for word in english_words)
         print("Tokens: ", tokens)
 
 tokenize(filename)
