72755e5b0e305b2696990adcde15efb7e3da0e75
Adit Mohindra <aditmohindra@gmail.com>
2023-10-11 15:07:21 -0700

    reads file but still need to make it only alnum

--- PartA.py
+++ PartA.py
@@ -3,10 +3,11 @@ import sys
 filename = sys.argv[1]
 
 def tokenize(filename):
-    with open(filename,'r') as data_file:
+    tokens = []
+    with open(filename,'r', encoding='utf-8') as data_file:
         for line in data_file:
             data = line.split()
-            # print(data)
-        print(data_file.read())
+            tokens.extend(word.lower() for word in data)  # Convert each word to lowercase
+        print("Tokens: ", tokens)
 
 tokenize(filename)
