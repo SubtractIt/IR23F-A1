517c526c6ec223bbb9b165181c0336ad0e04fc51
Adit Mohindra <aditmohindra@gmail.com>
2023-10-11 15:36:57 -0700

    reads tokens in list. deals w non alNums, non-eng

--- PartA.py
+++ PartA.py
@@ -7,9 +7,13 @@ def tokenize(filename):
     tokens = []
     with open(filename, 'r', encoding='utf-8') as data_file:
         for line in data_file:
-            # Use regular expression to match English words
-            english_words = re.findall(r'\b[a-zA-Z]+\b', line)
+            # Use regular expression to match English words and numbers
+            english_words = re.findall(r'\b[a-zA-Z0-9]+\b', line)
             tokens.extend(word.lower() for word in english_words)
-        print("Tokens: ", tokens)
+        # print("Tokens: ", tokens)
 
-tokenize(filename)
+    return tokens
+
+
+tokens = tokenize(filename)
+print(tokens)
